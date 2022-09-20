56f6ad0aec187bb406aeef9b31c53e37fd1a412d
Jeremiah Kendl <kendljeremiah@gmail.com>
2022-09-19 21:50:59 -0500

    9/19/22
    late night changes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const data = require('../develop/db/db.json');
 const { addNote } = require('../lib/data');
 
-
 router.get('/notes', (req, res) => {
     res.send(data);
 });
